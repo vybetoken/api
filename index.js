f97e1143c728970a85d55b3bf641cfbdc037ac21
Ty Schenk <tyschenk2@me.com>
2020-10-20 16:27:56 -0700

    change port

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ require('babel-register');
 // eslint-disable-next-line import/no-commonjs
 let { app } = require('./server');
 
-app.listen(80, () => console.log(`Vybe API listening on port 80!`));
+app.listen(8000, () => console.log(`Vybe API listening on port 8000!`));
